eaf1a91f52f115ba3db31b334b70ee988e1523c3
PascoPrzyborowski <unity01@gmx.net>
2022-08-18 09:47:17 +0200

    ex7_pep8_style

---
+++ pep8_style_ex1.py
@@ -0,0 +1,14 @@
+
+
+#ohne pep8 style
+
+
+
+
+
+#mit pep8  style
+ausgabetext = "Der Preis für 2 Socken beträgt 5 DM und 5 Paar kosten 10 DM"
+print(ausgabetext)
+ausgabetext = ausgabetext.replace("DM", "Euro")
+print("Nach dem Austauschen über replace():")
+print(ausgabetext)
