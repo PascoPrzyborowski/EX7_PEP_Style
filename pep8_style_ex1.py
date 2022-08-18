77511088d1687776f6c985071ef1153e35ab3825
PascoPrzyborowski <unity01@gmx.net>
2022-08-18 12:39:02 +0200

    ex7

--- pep8_style_ex1.py
+++ pep8_style_ex1.py
@@ -1,14 +1,43 @@
-
+import time
 
 #ohne pep8 style
-
-
+print()
+x = "Arbeitsbeschaffungsmakro für Word oder Excel. Ersetze A durch E und umgekehrt! viel Spaß im Büro mit langen Texten wieder rück zu verwandeln!"
+print(x)
+time.sleep(1)
+x = x.replace("a", "e")
+x = x.replace("e", "a")
+x = x.replace("A", "E")
+x = x.replace("E", "A")
+y = x
+print()
+print("Text nach dem Austauschen :")
+print()
+print(y)
+print()
+time.sleep(1)
 
 
 
 #mit pep8  style
-ausgabetext = "Der Preis für 2 Socken beträgt 5 DM und 5 Paar kosten 10 DM"
-print(ausgabetext)
-ausgabetext = ausgabetext.replace("DM", "Euro")
-print("Nach dem Austauschen über replace():")
-print(ausgabetext)
+print()
+ausgabetext = "Arbeitsbeschaffungsmakro für Word oder Excel \
+               Ersetze A durch E und umgekehrt! \
+               viel Spaß im Büro mit langen Texten wieder rück zu verwandeln!"
+               #Ausgabetext unverändert.
+
+print(ausgabetext) # Ausgabe
+time.sleep(1)
+
+ausgabetext = ausgabetext.replace("a", "e")
+ausgabetext = ausgabetext.replace("e", "a")
+ausgabetext = ausgabetext.replace("A", "E")
+ausgabetext = ausgabetext.replace("E", "A")
+# Ersetze a durch e im text und umgekehrt für Groß und Kleinschreibung (Blockanweisung)
+
+print()
+print("Text nach dem Austauschen :")
+print()
+print(ausgabetext) #Ausgabe veränderter Text
+print()
+time.sleep(1)
